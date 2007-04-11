a0464b71bf9f937eb8f819a64d93ad21b4d47059
Tony Stoecker <tony.stoecker@dzne.de>
2007-04-11 08:01:56 +0000

    1st functioning version - added setNLPs() - added comments (doxygen-style)
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@11 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- cpp/SEQ_FRAMEWORK/SincRfPulseShape.h
+++ cpp/SEQ_FRAMEWORK/SincRfPulseShape.h
@@ -10,11 +10,15 @@
 
 #include "RfPulseShape.h"
 
+//! symmetric apodized sinc-shaped RF pulse
+/*! 
+ *  Formulas from Bernstein et al., Hadnbook of MRI Pulse Sequences, p. 39
+ */
 class SincRfPulseShape :public RfPulseShape{
 
 public:
-   SincRfPulseShape (double dFlipAngle=90.0, double dPhase=0, double dBW=10,
-		     int iN=2 double dalpha=0.5, string sName="SincRfPulseShape" ) {
+   SincRfPulseShape (double dFlipAngle=90.0, double dPhase=0.0, double dBW=10.0,
+		     int iN=2, double dalpha=0.5, string sName="SincRfPulseShape" ) {
 	setName(sName);
 	setFlipAngle(dFlipAngle) ;
 	setPhase (dPhase);
@@ -24,20 +28,33 @@ public:
  	m_dalpha=dalpha;
    };
 
-// magnitude calculation
+  ~SincRfPulseShape(){};
+
+//! perform amplitude calculation from flip angle
   bool Prepare(bool verbose){
 	double t0=1.0/m_dBW, Sum = 0.0, DeltaT = getDuration()/10000.0 ;
 	for (double t=-m_iN*t0; t<=m_iN*t0; t+=DeltaT)
 	{
-		sinct = ( t==0.0 ? 1.0 : sin(PI*t/t0)/(PI*t/t0) );
+		double sinct = ( t==0.0 ? 1.0 : sin(PI*t/t0)/(PI*t/t0) );
 		Sum += (1.0-m_dalpha+m_dalpha*cos(PI*t/(m_iN*t0)))*sinct;
 	}
-	m_dAamplitude = getFlipAngle()*PI/(DeltaT*Sum*180.0);
+	m_dAmplitude = getFlipAngle()*PI/(DeltaT*Sum*180.0);
+	setNLPs();
  	return true;
   };
 
-  ~SincRfPulseShape(){};
-
+//! set the number of NLPs to eight times the number of zeros
+ void setNLPs() {
+  	m_iNumNLPs=8*m_iN+1;
+	double t0=1.0/m_dBW;
+	for (int i=0;i<4*m_iN;i++)
+	{
+		m_dArrayOfNLPs[i]=0.25*i*t0;
+		m_dArrayOfNLPs[i+4*m_iN+1]=0.25*(i+4*m_iN+1)*t0;
+	}
+	m_dArrayOfNLPs[4*m_iN]=m_iN*t0;
+ };
+ 
   void getValue(double * dAllVal, double const time,int const iLoop){
 	double dT=getDuration();
 	if ( time >= 0 && time <= dT )
@@ -45,7 +62,7 @@ public:
 		double t0=1.0/m_dBW;
 		double t=time-m_iN*t0;
 		double sinct = ( t==0.0 ? 1.0 : sin(PI*t/t0)/(PI*t/t0) );
-		double dVal = m_dAamplitude*(1.0-m_dalpha+m_dalpha*cos(PI*t/(m_iN*t0)))*sinct;
+		double dVal = m_dAmplitude*(1.0-m_dalpha+m_dalpha*cos(PI*t/(m_iN*t0)))*sinct;
 		dAllVal[0] += fabs(dVal);
 		dAllVal[1] += (dVal<0.0?PI:0.0);
 		dAllVal[1] += getPhase() * PI / 180.0;
@@ -53,7 +70,13 @@ public:
   };
 
 private:
- double m_dAamplitude, m_dalpha, m_dBW;
+ //! maximum amplitude of the sinc
+ double m_dAmplitude;
+ //! apodization factor (default 0.5 = Hamming window)
+ double m_dalpha;
+ //! bandwidth of the pulse
+ double m_dBW;
+ //! half the number of zero crossings of the pulse
  int m_iN;
 
 };
