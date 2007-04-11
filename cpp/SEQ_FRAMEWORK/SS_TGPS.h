55ac6aa08369505919d1096d7dc87a695495528e
Tony Stoecker <tony.stoecker@dzne.de>
2007-04-11 17:29:32 +0000

    corrected Prepare()
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@26 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- cpp/SEQ_FRAMEWORK/SS_TGPS.h
+++ cpp/SEQ_FRAMEWORK/SS_TGPS.h
@@ -9,13 +9,16 @@
 #define _SS_TGPS_H_
 
 #include "TGPS.h"
+#include "AtomicSequence.h"
 
+//! slice selection trapezoidal gradient pulse shape
 class SS_TGPS :public TGPS{
 public:
  //create with area, flat-top-time, number of ADCs, Axis, and name
- SS_TGPS(PulseShape* pPulse, PulseAxis eAxis=AXIS_GZ, string sName="SS_TGPS" ) {
+ SS_TGPS(PulseAxis eAxis=AXIS_GZ, double slicethickness=1000.0, string sName="SS_TGPS" ) {
  	setName(sName);
 	setAxis(eAxis);
+	m_dSliceThickness=slicethickness;
 	m_bVerbose=false;
 	Prepare(m_bVerbose);
  };
@@ -24,8 +27,35 @@ public:
 
 
   bool Prepare(bool verbose){
-	return true;
+        m_bVerbose=verbose;
+	double duration=0.0, bw=0.0;
+	AtomicSequence* pAS = ((AtomicSequence*) getAtomicSeq() );
+	if (pAS!=NULL)
+	{
+		if (m_dSliceThickness<0.0)
+		switch ( getAxis() )
+		{
+			case AXIS_GX : m_dSliceThickness = getAtomicSeq()->getRoot()->getParameter()->getDx(); break;
+			case AXIS_GY : m_dSliceThickness = getAtomicSeq()->getRoot()->getParameter()->getDy(); break;
+			case AXIS_GZ : m_dSliceThickness = getAtomicSeq()->getRoot()->getParameter()->getDz(); break;
+			default: cout << "?? wrong axis ??" << endl; return false;
+		}
+		for ( int i=0;i<pAS->getNumberOfPulses() ; i++ )
+			if ( pAS->getPulse(i)->getAxis() == AXIS_RF )
+				{
+					duration = pAS->getPulse(i)->getDuration();
+					bw = ((SincRfPulseShape*) pAS->getPulse(i))->getBandWidth();
+					break;
+				}
+	}
+	setArea(PI*bw*duration/m_dSliceThickness);
+	setDuration(duration);
+//cout << "SS_TGPS: " << getDuration() << " " << duration << " " << m_dSliceThickness << endl;
+	return (fabs(duration-getDuration())<0.0001);
  };
+
+private: 
+	double m_dSliceThickness;
 
 };//end of Class
 
