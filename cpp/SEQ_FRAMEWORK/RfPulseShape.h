9bed55fb9368de469be043c77081aa0be97749bb
Tony Stoecker <tony.stoecker@dzne.de>
2007-04-17 12:30:53 +0000

    added bandwidth
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@63 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- cpp/SEQ_FRAMEWORK/RfPulseShape.h
+++ cpp/SEQ_FRAMEWORK/RfPulseShape.h
@@ -14,23 +14,30 @@
 class RfPulseShape :public PulseShape{
 
 public:
-   RfPulseShape (){ setAxis(AXIS_RF); setPhaseLock(false); };
+   RfPulseShape (){ 	m_dBW=0.0; setAxis(AXIS_RF); setPhaseLock(false); };
   ~RfPulseShape(){};
    
   virtual void getValue(double * dAllVal, double const time,int const iLoop){};
 
-  double getFlipAngle ( ){return m_dFlipAngle;}; 
-  void setFlipAngle (double value ){m_dFlipAngle= value;};
-  double getPhase ( ){return m_dPhase;}; 
-  void setPhase (double value ){m_dPhase= value;};
+ double getFlipAngle ( ){return m_dFlipAngle;}; 
+ void setFlipAngle (double value ){m_dFlipAngle= value;};
+ double getPhase ( ){return m_dPhase;}; 
+ void setPhase (double value ){m_dPhase= value;};
 
  bool getPhaseLock(){return m_bPhaseLock;};
  void setPhaseLock(bool bval){m_bPhaseLock=bval;};
+
+ double getBandWidth(){return m_dBW;}
+
+
 protected:
 
-  double	m_dFlipAngle;
-  double	m_dPhase;
-  bool		m_bPhaseLock;
+ double	m_dFlipAngle;
+ double	m_dPhase;
+ bool		m_bPhaseLock;
+ //! bandwidth of the pulse
+ double m_dBW;
+ 
 
 public:
 };
