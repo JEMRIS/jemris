aa0fc84662c859e3fc360921dac37eaf388fba10
Kaveh Vahedipour <kaveh@codeare.org>
2007-04-24 06:56:19 +0000

    comments. better readability.
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@93 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- cpp/SEQ_FRAMEWORK/XmlSequence.h
+++ cpp/SEQ_FRAMEWORK/XmlSequence.h
@@ -1,8 +1,8 @@
 /*
-        This file is part of the MR simulation project
-        Date: 03/2006
-        Authors:  T. Stoecker, J. Dai
-        MR group, Institute of Medicine, Research Centre Juelich, Germany
+  This file is part of the MR simulation project
+  Date: 03/2006
+  Authors:  T. Stoecker, J. Dai
+  MR group, Institute of Medicine, Research Centre Juelich, Germany
 */
 
 #ifndef _XML2SEQ_H_
@@ -24,67 +24,81 @@ XERCES_CPP_NAMESPACE_USE
 
 
 class XmlSequence {
+	
+ public:
+
+	//constructor parses xml sequence file to a new DOM
+	XmlSequence(string xmlfile){
+
+		//initialize xerces DOM parser
+		XMLPlatformUtils::Initialize();
+		m_pXDP = new XercesDOMParser();
+
+		//check for file access
+		m_bOK=ifstream(xmlfile.c_str());
+
+		if (m_bOK) {
+
+			try { m_pXDP->parse( xmlfile.c_str() ); }
+			catch (const XMLException& toCatch) {//DOM parsing failed, exiting
+				m_bOK=false; return; }
+
+			m_nSeq = (DOMNode*)m_pXDP->getDocument();
+			m_nSeq = m_nSeq -> getFirstChild();
+			if (m_nSeq==NULL) {                  //root element is NULL, exiting
+				m_bOK=false; return;} 
+
+			string name = XMLString::transcode(m_nSeq->getNodeName()) ;
+			m_bOK = (name=="ConcatSequence");
+		}
+	};
+	
+	//decon deletes DOM and terminates xerces-c 
+	~XmlSequence ()    { delete m_pXDP; XMLPlatformUtils::Terminate(); };
+	
+	bool Status  ()    { return m_bOK; };
+	
+	//public function to get the sequence object
+	ConcatSequence* getSequence (bool verbose = true);
+
  
-public:
- //constructor parses xml sequence file to a new DOM
- XmlSequence(string xmlfile){
-	XMLPlatformUtils::Initialize();
-	m_pXDP = new XercesDOMParser();
-	m_bOK=ifstream(xmlfile.c_str());
-	if (m_bOK)
-	{
-		try { m_pXDP->parse( xmlfile.c_str() ); }
-		catch (const XMLException& toCatch) { m_bOK=false; return; }
-		m_nSeq = (DOMNode*)m_pXDP->getDocument();
-		m_nSeq = m_nSeq -> getFirstChild();
-		if (m_nSeq==NULL) {m_bOK=false; return;} 
-		string name = XMLString::transcode(m_nSeq->getNodeName()) ;
-		m_bOK = (name=="ConcatSequence");
-	}
- };
-
- //decon deletes DOM and terminates xerces-c 
- ~XmlSequence(){ delete m_pXDP; XMLPlatformUtils::Terminate(); };
-
- bool Status(){return m_bOK; };
-
- //public function to get the sequence object
- ConcatSequence* getSequence(bool verbose = true);
-
- 
-protected:
-
- DOMNode* getRoot(){ return m_nSeq; };
-
-//basic function: transforms DOM sequence to new created Sequence object
- Sequence* Transform(DOMNode* node);
-
- //every sequence class needs to have a create function
- void CreateConcatSequence(Sequence** pSeq, DOMNode* node);
- void CreateAtomicSequence(Sequence** pSeq, DOMNode* node);
- void CreateDelayAtom(Sequence** pSeq, DOMNode* node);
- void CreateGradientSpiralExtRfConcatSequence(Sequence** pSeq, DOMNode* node);
-
- //every pulseshape class needs to have a create function
- void CreatePulseShape(PulseShape** pPulse, int* iTreeSteps, DOMNode* node);
- void CreateEmptyPulse(PulseShape** pPulse, DOMNode* node);
- void CreateExternalPulseShape(PulseShape** pPulse, DOMNode* node);
- void CreateSincRfPulseShape(PulseShape** pPulse, DOMNode* node);
- void CreateHardRfPulseShape(PulseShape** pPulse, DOMNode* node);
- void CreateRfReceiverPhase(PulseShape** pPulse, DOMNode* node);
- void CreateRfPhaseCycling(PulseShape** pPulse, DOMNode* node);
- void CreateRfSpoiling(PulseShape** pPulse, DOMNode* node);
- void CreateTGPS(PulseShape** pPulse, DOMNode* node);
- void CreateRO_TGPS(PulseShape** pPulse, DOMNode* node);
- void CreatePE_TGPS(PulseShape** pPulse, DOMNode* node);
- void CreateSS_TGPS(PulseShape** pPulse, DOMNode* node);
- void CreateGradientSpiral(PulseShape** pPulse, DOMNode* node);
-
-private:
+ protected:
+	
+	DOMNode* getRoot() { return m_nSeq; };
+	
+	//basic function: transforms DOM sequence to new created Sequence object
+	Sequence* Transform (DOMNode* node);
+	
+	//every sequence class needs to have a create function
+	void CreateConcatSequence              (Sequence** pSeq, DOMNode* node);
+	void CreateAtomicSequence              (Sequence** pSeq, DOMNode* node);
+	void CreateDelayAtom                   (Sequence** pSeq, DOMNode* node);
+	void CreateGradientSpiralExtRfConcatSequence 
+		(Sequence** pSeq, DOMNode* node);
+	
+	//every pulseshape class needs to have a create function
+	void CreatePulseShape 
+		(PulseShape** pPulse, int* iTreeSteps, DOMNode* node);
+	void CreateEmptyPulse                  (PulseShape** pPulse, DOMNode* node);
+	void CreateExternalPulseShape          (PulseShape** pPulse, DOMNode* node);
+	void CreateSincRfPulseShape            (PulseShape** pPulse, DOMNode* node);
+	void CreateHardRfPulseShape            (PulseShape** pPulse, DOMNode* node);
+	void CreateRfReceiverPhase             (PulseShape** pPulse, DOMNode* node);
+	void CreateRfPhaseCycling              (PulseShape** pPulse, DOMNode* node);
+	void CreateRfSpoiling                  (PulseShape** pPulse, DOMNode* node);
+	void CreateTGPS                        (PulseShape** pPulse, DOMNode* node);
+	void CreateRO_TGPS                     (PulseShape** pPulse, DOMNode* node);
+	void CreatePE_TGPS                     (PulseShape** pPulse, DOMNode* node);
+	void CreateSS_TGPS                     (PulseShape** pPulse, DOMNode* node);
+	void CreateGradientSpiral              (PulseShape** pPulse, DOMNode* node);
+	
+ private:
 	XercesDOMParser* m_pXDP;
-	DOMNode* m_nSeq;	//the sequence represented as a DOM (given by the top node)
+
+	//the sequence represented as a DOM (given by the top node)
+	DOMNode* m_nSeq;	
+
 	bool m_bOK;
 };
-
 
 #endif
