5f6d6b01ca50b498290067a626b49240e1a708c8
Tony Stoecker <tony.stoecker@dzne.de>
2008-02-12 14:20:52 +0000

    added preliminary 10% uncertainty in spin-position of certesian grid (for 2D only so far)
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@491 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- cpp/MODEL/MR_Model.cpp
+++ cpp/MODEL/MR_Model.cpp
@@ -10,6 +10,11 @@
 //set the current spin of the sample
 void MR_Model::setSpin(int k){
 	initSolver();
+	double* dim = m_pSam->getSampleDim();
+	double d1=(dim[1]-dim[0]), d2=(dim[3]-dim[2]), d3=(dim[5]-dim[4]);
+        extern float gasdev(long *idum);
+	m_pSam->sSample.Xcoord[k] += 0.1*pow(d1*d2/m_pSam->sSample.NumberOfPoints,0.5)*gasdev(&m_i_rand_init);
+	m_pSam->sSample.Ycoord[k] += 0.1*pow(d1*d2/m_pSam->sSample.NumberOfPoints,0.5)*gasdev(&m_i_rand_init);
 	double dx=m_pSam->sSample.Xcoord[k];
 	double dy=m_pSam->sSample.Ycoord[k];
 	double dz=m_pSam->sSample.Zcoord[k];
