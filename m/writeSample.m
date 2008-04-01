7528150530819c3bda449cb492ccf8e8054bd763
Tony Stoecker <tony.stoecker@dzne.de>
2008-04-01 06:51:14 +0000

    2d and 3d implementation
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@497 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- m/writeSample.m
+++ m/writeSample.m
@@ -2,14 +2,22 @@ function writeSample(Sample)
 %
 
 N=size(Sample.M0);
-for i=1:3
+n=length(N);
+for i=1:n
     if mod(N(i),2)
         x{i}=[1:N(i)]-(N(i)-1)/2-1;
     else
         x{i}=[1:N(i)]-N(i)/2;
     end
 end
-[X,Y,Z]=meshgrid(Sample.dx*x{1},Sample.dy*x{2},Sample.dz*x{3});
+
+if n==2
+ [X,Y]=meshgrid(Sample.dx*x{1},Sample.dy*x{2}); Z=0*X;
+end
+
+if n==3
+ [X,Y,Z]=meshgrid(Sample.dx*x{1},Sample.dy*x{2},Sample.dz*x{3});
+end
 
 I=find(Sample.M0);
 N=length(I);
