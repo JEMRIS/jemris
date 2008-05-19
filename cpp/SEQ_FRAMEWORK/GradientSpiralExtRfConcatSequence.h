e6dfc81fe7eb8c8aab4352807b028c90c672158e
Tony Stoecker <tony.stoecker@dzne.de>
2008-05-19 13:18:36 +0000

    ???
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@520 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- cpp/SEQ_FRAMEWORK/GradientSpiralExtRfConcatSequence.h
+++ cpp/SEQ_FRAMEWORK/GradientSpiralExtRfConcatSequence.h
@@ -23,17 +23,15 @@ class  GradientSpiralExtRfConcatSequence :public ConcatSequence {
 	GradientSpiralExtRfConcatSequence(string filename, double dturns=32.0,
 									  double dtune=0.2, double dres=1.0,
 									  string name="GradientSpiralExtRfConcatSequence") {
-		Create(filename, dturns, dtune, dres, name); Prepare(false); 
-	};
-	//destructor
-	~GradientSpiralExtRfConcatSequence(){ };
+//cout << "GSERF bhhgng 0 " << endl;
+		//CreateS(filename, dturns, dtune, dres, name);
 
 
-	void Create(string filename, double dturns, double dtune, double dres, string name) {
+	//void CreateS(string filename, double dturns, double dtune, double dres, string name) {
+//cout << "GSERF bhhgng 1 " << endl;
 		//create concat sequences 
 		ConcatSequence* C = this;
 		C->setName(name);
-//cout << "GSERF bhhgng 1 " << endl;
 		
 		ExternalPulseShape* pEPS = new ExternalPulseShape(filename);
 		int     iparts   = pEPS->getNumOfNLPs();
@@ -43,27 +41,28 @@ class  GradientSpiralExtRfConcatSequence :public ConcatSequence {
 		double  dRFdur   = ddur/iparts;
 		
 //cout << "GSERF bhhgng 2 " << iparts << endl;
-		AtomicSequence** A_Parts = new AtomicSequence*[iparts];
+		AtomicSequence* A_before = NULL ;
+		AtomicSequence* A_after  = NULL ;
 		for (int i=0; i<iparts; ++i) {
 			//cout << "inserted part " << i ;
-			A_Parts[i] = new AtomicSequence("A_Segment");
+			A_after = new AtomicSequence("A_Segment");
 			//cout << "! " ;
-			A_Parts[i]->setPulse(new HardRfPulseShape(dRFamplt[i]*dRFdur*180.0/PI, dRFphase[i]*180.0/PI, dRFdur, "RF" ) );
-			A_Parts[i]->setPulse(new GradientSpiralPart(iparts, i+1, ddur, dturns, dtune, dres, AXIS_GX) );
-			A_Parts[i]->setPulse(new GradientSpiralPart(iparts, i+1, ddur, dturns, dtune, dres, AXIS_GY) );
+			A_after->setPulse(new HardRfPulseShape(dRFamplt[i]*dRFdur*180.0/PI, dRFphase[i]*180.0/PI, dRFdur, "RF" ) );
+			A_after->setPulse(new GradientSpiralPart(iparts, i+1, ddur, dturns, dtune, dres, AXIS_GX) );
+			A_after->setPulse(new GradientSpiralPart(iparts, i+1, ddur, dturns, dtune, dres, AXIS_GY) );
 			//cout << "! " ;
-			if (i==0) { C->InsertChild(NULL,A_Parts[i]); }
-			else	  { C->InsertChild(A_Parts[i-1],A_Parts[i]); }
+			C->InsertChild(A_before,A_after); 
+			A_before = A_after;
 			//cout << "! " ;
-			RfPulseShape* pRF = ((RfPulseShape*) A_Parts[i]->getPulse(0));
+			RfPulseShape* pRF = ((RfPulseShape*) A_after->getPulse(0));
 			//cout << "! " << endl;
-			//cout << "inserted part " << i << " , dur = " << ((GradientSpiralPart*) (A_Parts[i]->getPulse(0)))->getDuration()
-			// << "   RF = (" << pRF->getFlipAngle() << "," << pRF->getPhase() << ")" << endl;
+			//cout << "inserted part " << i << " , dur = " << ((GradientSpiralPart*) (A_after->getPulse(0)))->getDuration()
+			 //<< "   RF = (" << pRF->getFlipAngle() << "," << pRF->getPhase() << ")" << endl;
 		}
-		delete A_Parts; delete pEPS;
-//cout << "GSERF bhhgng 3 " << endl;
-		double dAreaX = ((GradientSpiralPart*) A_Parts[0]->getPulse(1))->getGS()->getArea();
-		double dAreaY = ((GradientSpiralPart*) A_Parts[0]->getPulse(2))->getGS()->getArea();
+		delete pEPS;
+cout << "GSERF bhhgng 3 " << endl;
+		double dAreaX = ((GradientSpiralPart*) A_after->getPulse(1))->getGS()->getArea();
+		double dAreaY = ((GradientSpiralPart*) A_after->getPulse(2))->getGS()->getArea();
 		AtomicSequence* A_reph = new AtomicSequence("A_rephaser");
 		A_reph->setPulse(new TGPS(-1.0*dAreaX, AXIS_GX, "GRephase_X") );
 		A_reph->setPulse(new TGPS(-1.0*dAreaY, AXIS_GY, "GRepahse_Y") );
@@ -71,9 +70,13 @@ class  GradientSpiralExtRfConcatSequence :public ConcatSequence {
 		((GradientPulseShape*) A_reph->getPulse(0))->setMaxAmpl(10.0);
 		((GradientPulseShape*) A_reph->getPulse(1))->setSlewRate(100.0);
 		((GradientPulseShape*) A_reph->getPulse(1))->setMaxAmpl(10.0);
-		C->InsertChild(A_Parts[iparts-1],A_reph);
+		C->InsertChild(A_before,A_reph);
 		
-	};//end of Create() function
+		Prepare(false); 
+	};
+	//destructor
+	~GradientSpiralExtRfConcatSequence(){ };
+	//};//end of Create() function
 
 };//end of class
 
