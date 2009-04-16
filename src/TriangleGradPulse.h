7e2724b3cdb051713a55d50c02a1e999ed5acfc7
Kaveh Vahedipour <kaveh@codeare.org>
2009-04-16 11:23:40 +0000

    JEMRIS MERGED
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@1140 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

---
+++ src/TriangleGradPulse.h
@@ -0,0 +1,87 @@
+/** @file TriangleGradPulse.h
+ *  @brief Implementation of TriangleGradPulse
+ *
+ * Author: tstoecker
+ * Date  : Apr 6, 2009
+ */
+
+/*
+ *  JEMRIS Copyright (C) 2007-2009  Tony Stöcker, Kaveh Vahedipour
+ *                                  Forschungszentrum Jülich, Germany
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#ifndef TRIANGLEGRADPULSE_H_
+#define TRIANGLEGRADPULSE_H_
+
+#include "GradPulse.h"
+
+enum TriangleType {TRIANGLE_UP, TRIANGLE_DN};
+/**
+ * @brief Gradient with constant amplitude
+ */
+class TriangleGradPulse : public GradPulse {
+
+
+public:
+
+    /**
+     * @brief Default constructor
+     */
+	TriangleGradPulse() {};
+
+    /**
+     * @brief Default copy constructor.
+     */
+	TriangleGradPulse  (const TriangleGradPulse&);
+
+    /**
+     * @brief Default destructor.
+     */
+    ~TriangleGradPulse () {};
+
+    /**
+     * See Module::clone
+     */
+    inline TriangleGradPulse* Clone() const {return (new TriangleGradPulse(*this));};
+
+    /**
+     * See Module::Prepare
+     */
+    virtual bool        Prepare  (PrepareMode mode);
+
+    /**
+     * See GradPulse::GetValue
+     */
+    virtual double GetGradient (double const time);
+
+ protected:
+
+     /**
+     * @brief Get informations on this triangle gradient
+     *
+     * @return GradPulse::GetInfo plus info of the Shape
+     */
+    virtual string          GetInfo        ();
+
+    double			m_amplitude;       /**< My amplitude      */
+    TriangleType	m_triangle_type;   /**< My type (TRIANGLE_UP or TRIANGLE_DN) */
+
+};
+
+
+#endif /* TRIANGLEGRADPULSE_H_ */
