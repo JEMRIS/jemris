621282acf02552b7201e1f05a56d24c0d15e14a3
Kaveh Vahedipour <kaveh@codeare.org>
2010-02-19 21:24:05 +0000

    Just some last steps for Biot-Savarts
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@1481 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- src/BiotSavartLoop.cpp
+++ src/BiotSavartLoop.cpp
@@ -52,26 +52,12 @@ double BiotSavartLoop::GetSensitivity(double* position) {
 				 +      pow(position[YC]-m_position[YC],2)
 				 +      pow(position[ZC]-m_position[ZC],2));
 
-	/*double q = 5.0;
-
-	if (abs(position[XC]) < m_radius/q)
-		position[XC] = ((position[XC]<0) ? -m_radius:m_radius)/q;
-
-	if (abs(position[YC]) < m_radius/q)
-		position[YC] = ((position[YC]<0) ? -m_radius:m_radius)/q;
-
-	if (abs(position[ZC]) < m_radius/q)
-		position[ZC] = ((position[ZC]<0) ? -m_radius:m_radius)/q;*/
-
-
-	// Calculate off axis angle
-
 	// scalar product of negative position vector and sample point
 	double scapr = position[XC]*(-m_position[XC])
 				 + position[YC]*(-m_position[YC])
 				 + position[ZC]*(-m_position[ZC]);
 
-	// absolute values of the vetors
+	// absolute values of the vectors
 	double abspt = sqrt (pow(  position[XC],2) + pow(  position[YC],2) + pow(  position[ZC],2));
 	double absrd = sqrt (pow(m_position[XC],2) + pow(m_position[YC],2) + pow(m_position[ZC],2));
 
@@ -82,48 +68,32 @@ double BiotSavartLoop::GetSensitivity(double* position) {
 	double r     = dist * sin (angle);
 	// distance on axis
 	double x     = dist * cos (angle);
-	//cout << "r: " << r << " x:" << x << endl;
-
 
 	double alpha = r/a;
 	double beta  = x/a;
 	double gamma = x/r;
 	double Q     = pow  ((1.0+alpha),2) + pow (beta,2);
 
-	double k = sqrt(4*alpha/Q);
-	k = (isnan(k)?0:k);
+	double k     = sqrt(4*alpha/Q);
+	k            = (isnan(k)?0.0:k);
 
 	double Kk    = 1.0;
 	double Ek    = 1.0;
 
 	#ifdef HAVE_BOOST
-	// Complete elliptical integral function of first  kind
-
-
-	Kk = boost::math::ellint_1(k, boost::math::policies::ignore_error);
-	// Complete elliptical integral function of second kind
-	Ek = boost::math::ellint_2(k, boost::math::policies::ignore_error);
-
+	// Complete elliptical integrals
+	Kk = boost::math::ellint_1(k);
+	Ek = boost::math::ellint_2(k);
 	#endif
 
 	double Bx    = (Ek * (1.0 - pow(alpha,2) - pow(beta,2)) / (Q-4.0*a) + Kk)         / (PI * sqrt(Q));
+	double Br    = (Ek * (1.0 + pow(alpha,2) + pow(beta,2)) / (Q-4.0*a) - Kk) * gamma / (2 * pow(PI,2) * sqrt(Q)) ;
 
-	double Br    = (Ek * (1.0 + pow(alpha,2) + pow(beta,2)) / (Q-4.0*a) - Kk) * gamma / (PI * sqrt(Q));
-	Br           = (Br< 0?-Br:Br);
-	Br           = Br/6.2830;
-
-	cout << x << " "  << r << endl;
-
+	// TODO: Only xy component is needed!
 	double B1    = sqrt(pow(Bx,2)+pow(Br,2));
 
-	//if (B1>1000/m_norm) return 0.0;
-/*
-	if (GetName()=="C1") cout <<  "(" << setw(4) << position[XC] << "," << setw(4) << position[YC]
-							  << "," << setw(4) << position[ZC] << ") || angle = "
-							  << angle*180.0/PI << " : Kk=" << Kk << " : Ek=" << Ek
-							  << " : r=" << r << " : x=" << x << " : B1=" << B1 << endl;
-*/
-	B1 = (isnan(B1)? 0.0:B1);
+	B1 = (isnan(B1)? 0.5:B1);
+	B1 = (dist > m_radius*6.28? 0.0: B1);
 	return B1;
 
 }
