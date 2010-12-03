fd8a835603ec48601e947574f8e5f9ae40d0f83d
Tony Stoecker <tony.stoecker@dzne.de>
2010-12-03 08:03:57 +0000

    new pbs-script location
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@1604 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- share/matlab/pbs_script.sh
+++ share/matlab/pbs_script.sh
@@ -1,37 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+
 # Queue to submit to
-#PBS -q dque
+#PBS -q cpu
+
 # Job Limits
+#PBS -N pc_nm_db
 #PBS -l cput=300000000
+
 # Keep stout,-error in
 #PBS -k eo
+
 # Merge std error to stdout
 #PBS -j oe
+
 # Set the name of output file
 #PBS -o test
-#PBS -l nodes=8:ppn=4
-# send me email when job end
-##PBS -M t.stoecker@fz-juelich.de
-##PBS -m e
-# send me email when job aborts (with an error)
-##PBS -m a
-# do not rerun this job if it fails
+#PBS -l nodes=8:ppn=4:quad
+##PBS -l nodes=8:ppn=8:oct
+
+# Send me email when job end
+##PBS -M k.vahedipour@fz-juelich.de
+##PBS -m abe
+
+# Do not rerun this job if it fails
 #PBS -r n
-# export all my environment variables to the job
+
+# Export all my environment variables to the job
 #PBS -V
-#
 
+# Change to working directory
 cd $PBS_O_WORKDIR/
-
-#machinefile for MPI
-
-machinefile=`basename $PBS_NODEFILE.tmp`
-uniq $PBS_NODEFILE > $machinefile
-
-#one extra process for the (mostly sleeping) master
-
-processes=`wc $PBS_NODEFILE|awk '{print 1+$1}'`
-
-#start MPI program 
-mpirun.mpich -machinefile $machinefile -np $processes /apps/prod/misc/bin/pjemris simu.xml > .sim.out 
+# Start mpd ring run mpi program and exit mpd ring
+mpdboot --totalnum=`cat $PBS_NODEFILE | uniq | wc -l` -f $PBS_NODEFILE
+mpiexec -machinefile $PBS_NODEFILE -np `cat $PBS_NODEFILE | wc -l|awk '{print $1}'` /vol/bin/pjemris simu.xml > .sim.out
+mpdallexit
 
