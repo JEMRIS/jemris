fbed353fe408c2e70d107a868f81e74fe8b0377c
Kaveh Vahedipour <kaveh@codeare.org>
2011-01-11 11:11:11 +0000

    SpiralGradPulse and the allocation of large arrays
    git-svn-id: http://nmrsrv02/svn/JEMRIS/trunk@1628 c4b88e63-5c3c-0410-8ec6-f0ed36dbe951

--- src/SpiralGradPulse.cpp
+++ src/SpiralGradPulse.cpp
@@ -28,9 +28,6 @@ double            SpiralGradPulse::GetGradient (double const time)  {
 
 	long index = (long) floor (time / m_grad_samp_int);
 
-	if (index > m_samples-1)
-		index = m_samples-1;
-
 	return m_amps [index];
 
 }
@@ -78,10 +75,9 @@ bool              SpiralGradPulse::Prepare     (PrepareMode mode)   {
 
     }
 
-    btag = (GradPulse::Prepare(mode) && btag);
 
 	if (mode == PREP_VERBOSE) {
-
+		
 		double gamma          = 42576000.0;
 
 		double samp_int       = 1.0 / m_bw;
@@ -90,39 +86,39 @@ bool              SpiralGradPulse::Prepare     (PrepareMode mode)   {
 
 		m_pitch               = 1000 * m_arms / (2.0 * PI * m_fov);
 		m_beta                = gamma * m_slewrate / m_pitch;
-
+		
 		if (m_max_grad/1000 > max_grad_samp) { // Nyquist limit exceeded
 			cout << "\n warning in Prepare(1) of KVSPIRAL " << GetName() << endl;
 			cout << "Nyquist limit (" << max_grad_samp << ") exceeded." << endl;
 		}
 		
 		double time_of_switch = 0.0;
-
+		
 		m_samples             = GetDuration() / m_grad_samp_int;
-
+		
 		double time           = 0.0;
 		double phi            = 0.0;
 		double max_phi        = 0.0;
-
+		
 		double klast[2];
 		double k    [2];
 		double g    [2];
 		double gabs           = 0.0;
 
-		if (m_amps)
-			delete [] m_amps;
-
-		m_amps                = new double[m_samples];
-
+		if (!m_amps)
+			m_amps            = (double*) malloc ((m_samples+1)*sizeof(double));
+		
 		k[XC]                 = 0.0;
 		k[YC]                 = 0.0;
 		klast[XC]             = 0.0;
 		klast[YC]             = 0.0;
-
+		g[XC]                 = 0.0;
+		g[YC]                 = 0.0;
+		
 		m_amps[0]             = 0.0;
-
+		
 		for (long i = 0; i <= m_samples; i++) {
-
+			
 			time = (double) i * m_grad_samp_int / 1000;
 			
 			if ( time_of_switch == 0.0 ) // Limited slewrate
@@ -134,19 +130,19 @@ bool              SpiralGradPulse::Prepare     (PrepareMode mode)   {
 			k[YC]  = m_pitch * phi * cos(phi);
 			
 			if (i > 0) {
-
+				
 				g[XC]  = 10000000.0 * (k[XC] - klast[XC]) / m_fov * 2 * PI / (gamma * m_grad_samp_int);
 				g[YC]  = 10000000.0 * (k[YC] - klast[YC]) / m_fov * 2 * PI / (gamma * m_grad_samp_int);
-
-				m_amps[i] = 1.0e16 * (m_axis == AXIS_GX) ? g[XC] : g[YC];
+				
+				m_amps[i] = 0.0; //1.0e16 * (m_axis == AXIS_GX) ? g[XC] : g[YC];
 				
 				gabs   = sqrt (pow(g[XC],2) + pow(g[YC],2));
-
+				
 			}
-
+			
 			klast[XC] = k[XC];
 			klast[YC] = k[YC];
-
+			
 			// Maximum gradient reached?
 			if (gabs >= m_max_grad && time_of_switch == 0.0) { 
 				m_max_grad     = gabs;
@@ -155,24 +151,26 @@ bool              SpiralGradPulse::Prepare     (PrepareMode mode)   {
 			}
 			
 		}
-
+		
 		if (m_inward)  {
-
-			double* tmp = new double [m_samples]; 
-
+			
+			double tmp[m_samples]; 
+			
 			for (long i = 0; i < m_samples; i++)
 				tmp [m_samples-1 - i] = m_amps[i];
-
-			delete [] m_amps;
-			m_amps = tmp;
-
+			
+			for (long i = 0; i < m_samples; i++)
+				m_amps[i] = tmp [i];
+			
 		}
-
+		
 	}
 	
+    btag = (GradPulse::Prepare(mode) && btag);
+
     if (!btag && mode == PREP_VERBOSE)
         cout << "\n warning in Prepare(1) of KVSPIRAL " << GetName() << endl;
-
+	
     return btag;
 }
 
@@ -191,7 +189,7 @@ string          SpiralGradPulse::GetInfo() {
 /**********************************************************/
 SpiralGradPulse::~SpiralGradPulse ()  { 
 
-	if (m_amps)
-		delete [] m_amps;
+	//if (m_amps)
+	//delete [] m_amps;
 
 };
