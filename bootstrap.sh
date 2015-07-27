bf1e886b4011c80f51348c0edc552edb31ff289c
brennerd <daniel.brenner@me.com>
2015-07-27 19:49:14 +0200

    Added vagrant support. Allows automatic provisioning of VM that downloads all necessary components for JEMRIS. Clones the development git directory it was invoked from and installs JEMRIS. Initial tests are positive.

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+sudo apt-get install -y git g++ libboost-math-dev
+sudo apt-get install -y libhdf5-dev libhdf5-serial-dev
+sudo apt-get install -y libginac-dev libginac2 libginac2-dbg ginac-tools
+sudo apt-get install -y libxerces-c3.1 libxerces-c-dev libsundials-cvode1 libsundials-serial-dev
+sudo apt-get install -y mpich
+
+sudo echo \#!/bin/sh > /usr/local/bin/ginac-excompiler
+sudo echo gcc -x c -fPIC -shared -o $1.so $1 > /usr/local/bin/ginac-excompiler
+
+cd ~
+if [ -d "jemris_src" ]; then
+  sudo rm -rf jemris_src
+fi
+mkdir ~/jemris_src
+cd jemris_src
+git clone /vagrant .
+
+./configure
+make && make install
+
+jemris modlist 
