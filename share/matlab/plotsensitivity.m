8cfec9285c8f609499e85432f916125b54b8f493
stoeckert <tony.stoecker@dzne.de>
2022-02-04 09:38:09 +0100

    negative ADCs bugfix and cleaning

--- share/matlab/plotsensitivity.m
+++ share/matlab/plotsensitivity.m
@@ -5,7 +5,7 @@ function handles=plotsensitivity(handles,plot_coils_call)
 
 %
 %  JEMRIS Copyright (C)
-%                        2006-2019  Tony Stoecker
+%                        2006-2022  Tony Stoecker
 %                        2007-2015  Kaveh Vahedipour
 %                        2009-2019  Daniel Pflugfelder
 %
